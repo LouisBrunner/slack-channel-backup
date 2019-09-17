de0e77018afa42a113da31cb41c11f6976b5e5ce
Louis Brunner <louis.brunner.fr@gmail.com>
2019-09-17 18:22:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# slack-channel-backup
+
+This tool allows you to backup (and potentially delete) the content of a Slack channel (including files).
+
+## Usage
+
+You will need to provide your own `SLACK_API_TOKEN` token as a environment variable
+
+```
+$ export SLACK_API_TOKEN=xoxp-WHATEVER
+$ ./slack-backup.py '#random' --where mybackup
+```
+
+You will end up with a folder named `mybackup` containing a `conversation.txt` file with all the history and a `files` folder with the uploaded files (which are properly tagged as such in the history).
+
+The tool also allows to control a date range for the backup and to delete the backed up history:
+
+```
+$ ./slack-backup.py -h
+usage: slack-backup.py [-h] [--where WHERE] [--delete] [--from FROM] [--to TO] channel
+
+Backup a slack channel (and potentially delete) the content of a Slack channel (including files)
+
+positional arguments:
+  channel        channel to backup (use # prefix for channels, @ prefix for DMs)
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --where WHERE  where to store the backup
+  --delete       delete the backed up messages
+  --from FROM    when to start backing/deleting up
+  --to TO        when to stop backing/deleting up
+```
+
+## Required permissions
+
+You will need to [create a Slack app](https://api.slack.com/apps) with the following permissions:
+
+ - In every case: `files:read`, `users:read` (if deleting: `files:write:user`)
+ - When using public channels: `channels:history`, `channels:read` (if deleting: `channels:write`)
+ - When using private channels: `groups:history`, `groups:read` (if deleting: `groups:write`)
+ - When using Direct Messages: `im:history`, `im:read` (if deleting: `im:write`)
---
+++ slack-backup.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import sys
+from datetime import datetime
+
+
+import slack
+import requests
+from tqdm import tqdm
+
+
+def read_all_messages(client, func_name, channel, oldest=None, latest=None):
+    func = getattr(client, func_name)
+    args = {'channel': channel, 'count': 1000}
+    if oldest is not None:
+        args['oldest'] = oldest
+    if latest is not None:
+        args['latest'] = latest
+
+    messages = []
+    r = func(**args)
+    messages = r['messages']
+    while r['has_more']:
+        if latest is not None:
+            args['latest'] = messages[-1]['ts']
+        else:
+            args['oldest'] = messages[0]['ts']
+        r = func(**args)
+        if oldest is not None and latest is None:
+            messages[:0] = r['messages']
+        else:
+            messages.extend(r['messages'])
+    return messages
+
+def read_all(client, kind, func_name, get_display, find=None):
+    func = getattr(client, func_name)
+
+    r = func(limit=200)
+    raw_list = r[kind]
+    while r['response_metadata']['next_cursor'] != '':
+        r = func(limit=200, cursor=r['response_metadata']['next_cursor'])
+        raw_list.extend(r[kind])
+
+    list = {}
+    found = None
+    for raw in raw_list:
+        display_name = get_display(raw)
+        list[raw['id']] = display_name
+        if find == display_name:
+            found = raw['id']
+    return list, found
+
+def read_all_users(client, find_user=None):
+    return read_all(client, 'members', 'users_list', lambda u: u['profile']['display_name'], find_user)
+
+def read_all_channels(client, find_channel=None):
+    return read_all(client, 'channels', 'channels_list', lambda c: c['name'], find_channel)
+
+def parse_message(message, files_path, token):
+    what = message['text']
+    if what == '':
+        parts = []
+        if 'files' in message:
+            os.makedirs(files_path, exist_ok=True)
+
+            files = []
+            for file in message['files']:
+                name = f'{file["id"]}.{file["filetype"]}'
+                with open(os.path.join(files_path, name), 'wb') as f:
+                    r = requests.get(file['url_private'], headers={'Authorization': f'Bearer {token}'})
+                    f.write(r.content)
+                what_i = f'{{media}} <files/{name}>'
+                files.append(what_i)
+            parts.append(' & '.join(files))
+
+        if 'attachments' in message:
+            attachments = []
+            for attachment in message['attachments']:
+                what_i = f'{{attach}} {attachment["fallback"]}'
+                attachments.append(what_i)
+            parts.append(' & '.join(attachments))
+
+        what = ' + '.join(parts)
+
+    if 'thread_ts' in message:
+        what = '{thread} ' + what
+
+    return what
+
+def assert_arg(find, found, label):
+    if find is not None and found is None:
+        print(f'error: could not find {label} {find}')
+        sys.exit(1)
+
+def valid_date(s):
+    try:
+        return datetime.strptime(s, "%Y-%m-%d")
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+def main():
+    token = os.environ['SLACK_API_TOKEN']
+    default_folder = f'slack-backup-{datetime.now().strftime("%Y-%m-%dT%H-%M-%S")}'
+
+    parser = argparse.ArgumentParser(description='Backup a slack channel (and potentially delete) the content of a Slack channel (including files)')
+    parser.add_argument('channel', help='channel to backup (use # prefix for channels, @ prefix for DMs)')
+    parser.add_argument('--where', default=default_folder, help='where to store the backup')
+    parser.add_argument('--delete', action='store_true', help='delete the backed up messages')
+    parser.add_argument('--from', dest='frm', type=valid_date, help='when to start backing/deleting up')
+    parser.add_argument('--to', type=valid_date, help='when to stop backing/deleting up')
+    args = parser.parse_args()
+
+    root = args.where
+
+    client = slack.WebClient(token=token)
+
+    find_user = None
+    find_channel = None
+    prefix = args.channel[0]
+    if prefix == '@':
+        find_user = args.channel[1:]
+    elif prefix == '#':
+        find_channel = args.channel[1:]
+    users, found_user = read_all_users(client, find_user)
+    found_channel = None
+    if find_channel is not None:
+        _, found_channel = read_all_channels(client, find_channel)
+
+    assert_arg(find_user, found_user, 'user')
+    assert_arg(find_channel, found_channel, 'channel')
+
+    read_args = {}
+    if args.frm is not None:
+        read_args['oldest'] = str(args.frm.timestamp())
+    if args.to is not None:
+        read_args['latest'] = str(args.to.timestamp())
+
+    os.makedirs(root, exist_ok=True)
+    files_path = os.path.join(root, 'files')
+    messages = []
+    if found_user is not None:
+        conv = client.conversations_open(users=[found_user])
+        messages = read_all_messages(client, 'im_history', conv['channel']['id'], **read_args)
+    else:
+        messages = read_all_messages(client, 'channel_history', found_channel, **read_args)
+    with open(os.path.join(root, 'conversation.txt'), 'w') as f:
+        for message in tqdm(reversed(messages), total=len(messages)):
+            when = datetime.utcfromtimestamp(float(message['ts'])).strftime('%Y-%m-%d %H:%M:%S')
+            who = users.get(message['user'], '!Unknown!')
+            what = parse_message(message, files_path, token)
+            f.write(f'[{when}] {who}: {what}\n')
+
+if __name__ == "__main__":
+    main()
